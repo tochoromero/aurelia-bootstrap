e4a0f3d02298bd78060da2860cc9446c870da939
Hector Romero <hromerogranillo@oreillyauto.com>
2016-08-13 15:19:41 -0500

    Move buttons to its own directory

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 export function configure(config) {
-    
+    config.globalResources('./buttons/aubs-btn-checkbox');
+    config.globalResources('./buttons/aubs-btn-disabled');
+    config.globalResources('./buttons/aubs-btn-loading');
+    config.globalResources('./buttons/aubs-btn-radio');
 }
