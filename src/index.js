7b7e3715d2a5a0f6152f372f8a9f0588c83ad6c4
Hector Romero <hromerogranillo@oreillyauto.com>
2016-09-08 04:19:40 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 export function configure(config) {
+    config.globalResources('./accordion/aubs-accordion');
+    config.globalResources('./accordion/aubs-accordion-group');
     config.globalResources('./buttons/aubs-btn-checkbox');
     config.globalResources('./buttons/aubs-btn-disabled');
     config.globalResources('./buttons/aubs-btn-loading');
     config.globalResources('./buttons/aubs-btn-radio');
     config.globalResources('./dropdown/aubs-dropdown');
     config.globalResources('./dropdown/aubs-dropdown-toggle');
+    config.globalResources('./popover/aubs-popover');
+    config.globalResources('./popover/aubs-custom-popover');
+    config.globalResources('./tabs/aubs-tab');
+    config.globalResources('./tabs/aubs-tabset');
+    config.globalResources('./tabs/aubs-tooltip');
 }
