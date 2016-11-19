ee78ce1d8ee9c4d264125099da7c7a95085b94c4
Hector Romero <hromerogranillo@oreillyauto.com>
2016-11-19 16:05:22 -0600

    Collapse documentation

--- doc/src/app.js
+++ doc/src/app.js
@@ -6,6 +6,7 @@ export class App {
             {route: ['', 'introduction'], name: 'introduction', moduleId: 'introduction/introduction', nav: false},
             {route: ['accordion'], name: 'accordion', moduleId: 'accordion/accordion', nav: true, title: 'Accordion'},
             {route: ['buttons'], name: 'buttons', moduleId: 'buttons/buttons', nav: true, title: 'Buttons'},
+            {route: ['collapse'], name: 'collapse', moduleId: 'collapse/collapse', nav: true, title: 'Collapse'},
             {route: ['dropdown'], name: 'dropdown', moduleId: 'dropdown/dropdown', nav: true, title: 'Dropdown'},
             {route: ['popover'], name: 'popover', moduleId: 'popover/popover', nav: true, title: 'Popover'},
             {route: ['tabs'], name: 'tabs', moduleId: 'tabs/tabs', nav: true, title: 'Tabs'},
